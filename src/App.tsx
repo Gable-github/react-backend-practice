c85aec857fbc5dd64951861fdbdc70d96e73c958
Gabryel <gabryelsoh@gmail.com>
2024-04-03 17:22:31 +0800

    revising form select and using useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
-  return <div></div>;
+  const [category, setCategory] = useState("");
+
+  return (
+    <div>
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
+    </div>
+  );
 }
 
 export default App;
